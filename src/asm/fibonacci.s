10f0f392c030ca769053093d842701b4da925134
Saksham Gupta <sakshamdam54321@gmail.com>
2022-02-18 16:23:30 +0530

    Fix fibonacci program
    We can't load directly value 10 in R1, so first load in AX then R1
    
    We do not need to increment memory location by 2 since we are directly
    using int array for simulation
    
    We were not reducing R1 by 1 hence simulator was stuck in infinite loop

--- src/asm/fibonacci.s
+++ src/asm/fibonacci.s
@@ -1,16 +1,17 @@
 ; Calculate and store first n (stored in R1) Fibonacci numbers at memory location m (stored in R2)
-ADD R1 R0 #10
+LAX #10
+STX R1
 
-LAX #512
+LAX #0
 STX R2
 
 ADD R3 R0 R0
 ADD R4 R0 #1
 
 ST R0[R2] R3
-ADD R2 R2 #2
+ADD R2 R2 #1
 ST R0[R2] R4
-ADD R2 R2 #2
+ADD R2 R2 #1
 
 fibonacci:
 BEQZ R1 exit
@@ -18,7 +19,8 @@ ADD R5 R3 R4
 ADD R3 R0 R4
 ADD R4 R0 R5
 ST R0[R2] R4
-ADD R2 R2 #2
+ADD R2 R2 #1
+SUB R1 R1 #1
 JMP fibonacci
 
 exit:
